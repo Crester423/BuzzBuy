8410765bf6a531524ae0cc0c7d813c06763af7ca
Bhavya-Goyal423 <Goel2000bhavya@gmail.com>
2024-02-08 20:03:33 +0530

    added command to stop and resume ping

--- events/messageCreate/ticket.js
+++ events/messageCreate/ticket.js
@@ -1,22 +1,38 @@
 const ownerId = require("../../utils/owner");
 
+let blockChannel = [];
+
 module.exports = async (message, client) => {
-  if (message.author.bot) return;
   const channel = message.channel;
-  if (!channel.name.startsWith("ticket")) return;
-  const guild = message.guild;
+  if (message.content === "?startPing" && ownerId.includes(message.author.id)) {
+    message.reply("Ping resumed ✅");
+    blockChannel = blockChannel.filter((el) => el !== channel.name);
+  } else if (
+    message.content === "?stopPing" &&
+    ownerId.includes(message.author.id)
+  ) {
+    message.reply("Ping stopped ✅");
+    blockChannel.push(channel.name);
+  } else {
+    if (message.author.bot) return;
 
-  const memberStatus = [];
+    if (blockChannel.includes(channel.name)) return;
 
-  const fetchPromises = ownerId.map(async (member) => {
-    const curMember = await guild.members.fetch(member);
-    return curMember.presence?.status;
-  });
+    if (!channel.name.startsWith("ticket")) return;
+    const guild = message.guild;
 
-  memberStatus.push(...(await Promise.all(fetchPromises)));
-  if (memberStatus.every((val) => val === undefined || val === "offline")) {
-    message.reply(
-      "No admin is currently online, please wait and we will get back to you as soon as possible!"
-    );
-  } else return;
+    const memberStatus = [];
+
+    const fetchPromises = ownerId.map(async (member) => {
+      const curMember = await guild.members.fetch(member);
+      return curMember.presence?.status;
+    });
+
+    memberStatus.push(...(await Promise.all(fetchPromises)));
+    if (memberStatus.every((val) => val === undefined || val === "offline")) {
+      message.reply(
+        "No admin is currently online, please wait and we will get back to you as soon as possible!"
+      );
+    } else return;
+  }
 };
