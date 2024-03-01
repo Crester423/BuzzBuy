8453c2da30789e37bb2c68f158ab20dce324c6de
Bhavya-Goyal423 <Goel2000bhavya@gmail.com>
2024-03-01 19:19:03 +0530

    added prices for discord

--- commands/prices.js
+++ commands/prices.js
@@ -126,14 +126,14 @@ module.exports = {
       else if (subcommand === "nitro") {
         const exampleEmbed = {
           color: 0x0099ff,
-          description: `**Discord Nitro Monthly**\n\n**Per Month ► 240 INR / 2.99 USD**`,
+          description: `**Discord Nitro Monthly**\n\n**Per Month ► 450 INR / 5.49 USD**`,
           thumbnail: {
             url: "https://media.discordapp.net/attachments/751432960066060308/1180454130628702268/b941bc1dfe379db6cc1f2acc5a612f41.jpg?ex=65be1414&is=65ab9f14&hm=df5d484136190d727a5ebbb19b407e0c9a17d29b03e5c0b27f3d1fd5f479e103&=&format=webp",
           },
         };
         const embedTwo = {
           color: 0x0099ff,
-          description: `**Discord Nitro Yearly**\n\n**Per Year ► 2199 INR / 25.99 USD**`,
+          description: `**Discord Nitro Yearly**\n\n**Per Year ► 4500 INR / 54.99 USD**`,
           thumbnail: {
             url: "https://media.discordapp.net/attachments/751432960066060308/1180454130628702268/b941bc1dfe379db6cc1f2acc5a612f41.jpg?ex=65be1414&is=65ab9f14&hm=df5d484136190d727a5ebbb19b407e0c9a17d29b03e5c0b27f3d1fd5f479e103&=&format=webp",
           },
